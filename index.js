7d07156f007c82524f52ad0fdd2def346255b000
ayham <ayhamgm81@gmail.com>
2023-01-29 15:27:30 +0200

    new futures

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import isMobile from "is-mobile";
 if (!isMobile()) {
   console.log("heelo");
 }
+
+const good_boy = "ayham";
+const bad_boy = "diaa";
+
+const bros = good_boy + bad_boy;
+console.log(bros);
 console.log("hello guys :)");
 console.log("GG wp");
-console.log('diaa was here!!!');
+console.log("diaa was here!!!");
