837e9bf61afd0cd7c3cb59f2e1b8e9830baed2d3
Wziro1 <traptedy@gmail.com>
2023-01-29 15:22:46 +0200

    ana diaa

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import isMobile from "is-mobile";
 if (!isMobile()) {
   console.log("heelo");
 }
-console.log("hello :)");
+console.log("hello guys :)");
 console.log("GG wp");
 console.log('diaa was here!!!');
